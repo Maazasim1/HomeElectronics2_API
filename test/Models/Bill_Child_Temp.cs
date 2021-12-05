d053e1f5568157d2ffd7b720aabf70c4bfe80aa2
umair-dawood <93153139+umair-dawood@users.noreply.github.com>
2021-12-05 19:54:15 +0500

    Bill_Child_temp

---
+++ test/Models/Bill_Child_Temp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test.Models
+{
+    public class Bill_Child_Temp
+    {
+        public string ItemSKU { get; set; }
+        public string ItemBrand { get; set; }
+        public string ItemType { get; set; }
+        public float ItemPrice { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
